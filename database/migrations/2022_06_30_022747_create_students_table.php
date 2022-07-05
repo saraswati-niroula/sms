6097ff23ba0f19a2f76f3c45d55b8ab9017a6e1a
SaRu-N <saruniroula73@gmail.com>
2022-07-05 07:24:54 +0545

    student controller

---
+++ database/migrations/2022_06_30_022747_create_students_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name', 150);
+            $table->string('mobile', 10)->unique();
+            $table->string('citizenship', 20)->unique(); 
+            $table->string('gender', 1);
+            $table->string('blood_group', 3);
+            $table->string('perm_address');
+            $table->string('temp_address');
+            $table->string('email', 10);
+            $table->date('dob');
+            $table->string('picture')->nullable();
+            $table->boolean('is_active');
+            $table->boolean('is_almuni');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
