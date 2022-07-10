a9a116c31eb416828a514faef733155ea4f74d6f
SaRu-N <saruniroula73@gmail.com>
2022-07-10 21:30:17 +0545

    Student Registration Form

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -9,37 +9,98 @@
             <!-- general form elements -->
             <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Quick Example</h3>
+                <h3 class="card-title">Student Registration Form</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
               <form>
                 <div class="card-body">
                   <div class="form-group">
-                    <label for="exampleInputEmail1">Name</label>
+                    <label for="name">Name</label>
                     <input type="text" 
                     name="name"
-                    class="form-control" id="exampleInputEmail1" placeholder="Enter Name">
+                    class="form-control" id="name" placeholder="Enter Name">
                   </div>
                   <div class="form-group">
-                    <label for="exampleInputPassword1">Email</label>
+                    <label for="email">Email</label>
                     <input type="email"
                     name="email"
-                     class="form-control" id="exampleInputPassword1" placeholder="Enter Email">
+                     class="form-control" id="email" placeholder="Enter Email">
                   </div>
                   <div class="form-group">
-                    <label for="exampleInputPassword1">Phone</label>
+                    <label for="phone">Phone</label>
                     <input type="text"
                     name="phone"
-                     class="form-control" id="exampleInputPassword1" placeholder="Enter Mobile Number">
+                     class="form-control" id="phone" placeholder="Enter Mobile Number">
                   </div>
                   <div class="form-group">
-                    <label for="exampleInputPassword1">DOB</label>
+                    <label for="picture">Picture</label>
+                    <input type="file" accept="image/*" 
+                    name="picture"
+                     class="form-control" id="picture" 
+                     placeholder="choose a image" />
+                  </div>
+                  <div class="form-group">
+                    <label for="dob">DOB</label>
                     <input type="text"
                     name="dob"
-                     class="form-control" id="exampleInputPassword1" 
+                     class="form-control" id="dob" 
                      placeholder="Enter your DOB">
                   </div>
+                  <div class="form-group">
+                    <label for="gender">Gender</label>
+                    <input type="radio" name="gender" value="male"> Male
+                    <input type="radio" name="gender" value="female"> Female
+                  </div>
+                  <div class="form-group">
+                    <label for="citizenship">Citizenship</label>
+                    <input type="file" accept="image/*" 
+                    name="citizenship"
+                     class="form-control" id="citizenship" 
+                     placeholder="choose a image" />
+                  </div>
+                  <div class="form-group">
+                  <label for="blood_group">Choose your Blood Group</label>
+                    <select name="blood_group" id="blood_group" placeholder="Blood Group">
+                      <option value="A+">A+ve</option>
+                      <option value="A+">A-ve</option>
+                      <option value="A+">B+ve</option>
+                      <option value="A+">B-ve</option>
+                      <option value="A+">AB+ve</option>
+                      <option value="A+">AB-ve</option>
+                      <option value="A+">O+ve</option>
+                      <option value="A+">O-ve</option>
+                    </select> 
+                  </div>
+                  <div class="form-group">
+                    <label for="fathers_name">Father's Name</label>
+                    <input type="text"
+                    name="fathers_name"
+                     class="form-control" id="fathers_name" 
+                     placeholder="Enter your Fathers Name">
+                  </div>
+                  <div class="form-group">
+                    <label for="mothers_name">Mother's Name</label>
+                    <input type="text"
+                    name="mothers_name"
+                     class="form-control" id="mothers_name" 
+                     placeholder="Enter your Mothers Name">
+                  </div>
+                  <div class="form-group">
+                    <label for="p_address">Permanent Address</label>
+                    <input type="text"
+                    name="p_address"
+                     class="form-control" id="p_address" 
+                     placeholder="Enter your Permanent Address">
+                  </div>
+                  <div class="form-group">
+                    <label for="t_address">Temporary Address</label>
+                    <input type="text"
+                    name="t_address"
+                     class="form-control" id="t_address" 
+                     placeholder="Enter your Temporary Address">
+                  </div>
+             
                   <!-- <div class="form-group">
                     <label for="exampleInputFile">File input</label>
                     <div class="input-group">
